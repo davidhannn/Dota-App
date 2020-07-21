f16654b98b1b578ac30a818696f4a98879ed265a
David Han <email>
2020-07-20 19:07:42 -0700

    Second commit

--- routes/matches.js
+++ routes/matches.js
@@ -16,6 +16,7 @@ router.get("/", async (req, res, next) => {
   const urls = [
     `https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=5506574406&key=${apiKey}`,
     `https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=${apiKey}`,
+    `https://api.steampowered.com/IEconDOTA2_570/GetGameItems/V001/?key=${apiKey}`,
   ];
 
   const apiData = urls.map((url) => {
